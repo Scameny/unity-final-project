b09623b6641e1b2627286e4088934542ea4959eb
Scameny <aokker98@gmail.com>
2022-09-23 16:01:21 +0200

    Resource icon unit subscribed to UIManager

--- Assets/Scripts/UI/Resources/ResourceIconUnit.cs
+++ Assets/Scripts/UI/Resources/ResourceIconUnit.cs
@@ -1,31 +1,32 @@
 using Character.Character;
 using Character.Stats;
+using GameManagement;
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace UI.CharacterResources
 {
-    public class ResourceIconUnit : MonoBehaviour
+    public class ResourceIconUnit : MonoBehaviour, IObserver<SignalData>
     {
         public ResourceType resourceType;
 
         TextMeshProUGUI textElement;
         Image icon;
         DefaultCharacter player;
+        IDisposable disposable;
 
         private void Awake()
         {
             textElement = GetComponentInChildren<TextMeshProUGUI>();
             icon = GetComponentInChildren<Image>();
         }
-        private void Update()
-        {
-            UpdateResourceUnit();
-        }
 
+      
         private void Start()
         {
+            disposable = UIManager.manager.Subscribe(this);
             player = GameObject.FindGameObjectWithTag("Player").GetComponent<DefaultCharacter>();
         }
 
@@ -41,6 +42,34 @@ namespace UI.CharacterResources
                 textElement.gameObject.SetActive(true);
                 icon.gameObject.SetActive(true);
                 textElement.text = player.GetCurrentResource(resourceType).ToString();
+            }
+        }
+
+        public void OnCompleted()
+        {
+            disposable.Dispose();
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNext(SignalData value)
+        {
+            if (value.signal.Equals(GameSignal.START_COMBAT) || value.signal.Equals(GameSignal.START_GAME))
+            {
+                UpdateResourceUnit();
+            }
+            else if (value.signal.Equals(GameSignal.RESOURCE_MODIFY) && (value as CombatResourceSignalData).user.Equals(player.gameObject) &&
+                (value as CombatResourceSignalData).resourceType.Equals(resourceType))
+            {
+                UpdateResourceUnit();
+            }
+            else if (value.signal.Equals(GameSignal.OUT_OF_COMBAT_CURRENT_RESOURCE_MODIFY) && (value as ResourceSignalData).user.Equals(player.gameObject) &&
+                (value as ResourceSignalData).resourceType.Equals(resourceType))
+            {
+                UpdateResourceUnit();
             }
         }
     }
